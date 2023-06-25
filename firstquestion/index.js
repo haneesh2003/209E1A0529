1fd9ea6993c1be6b70bac8eb577f2a4328d48c79
Bungatavula Haneesh <131774122+haneesh2003@users.noreply.github.com>
2023-06-25 14:00:37 +0530

    updated the code

--- firstquestion/index.js
+++ firstquestion/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const axios = require('axios');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+const requestTimeout = 1000;
+const port = 8008;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/numbers', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL parameter is missing or invalid' });
+  }
+
+  const urls = Array.isArray(url) ? url : [url];
+  const requests = urls.map(fetchData);
+
+  try {
+    const responses = await Promise.all(requests);
+    const numbers = extractNumbers(responses);
+    const sortedNumbers = sortNumbers(numbers);
+
+    return res.json({ numbers: sortedNumbers });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+async function fetchData(url) {
+  try {
+    const response = await axios.get(url, { timeout: requestTimeout });
+    return response.data.numbers || [];
+  } catch (error) {
+    return [];  
+  }
+}
+
+function extractNumbers(responses) {
+  const numbers = [];
+  for (const response of responses) {
+    numbers.push(...response);
+  }
+  return Array.from(new Set(numbers));
+}
+
+function sortNumbers(numbers) {
+  return numbers.sort((a, b) => a - b);
+}
